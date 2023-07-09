d9c4ae0f432e9ba80631370076c58b86c53b5cc2
Keegan <keegs4213@gmail.com>
2023-07-09 22:35:59 +1200

    tidied code

--- front-end/app/components/SignupForm.jsx
+++ front-end/app/components/SignupForm.jsx
@@ -4,81 +4,81 @@ import styles from "./../../app/globals.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+const FormInput = ({label, type, value, onChange, className, name}) => (
+  <label className={styles.signupLabel}>
+    {label}:
+    <input type={type} name={name} value={value} onChange={onChange} className={className} />
+  </label>
+)
+
 function SignupForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [formState, setFormState] = useState({
+    username: '',
+    password: '',
+    email: '',
+  });
   const [message, setMessage] = useState("");
   const [isClient, setIsClient] = useState(false);
   const router = isClient ? useRouter() : undefined;
-
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
-  const handleUsernameChange = (event) => setUsername(event.target.value);
-  const handlePasswordChange = (event) => setPassword(event.target.value);
-  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setFormState(prevState => ({...prevState, [name]: value}));
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
     const response = await fetch("http://localhost:3245/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password, email }),
+      body: JSON.stringify(formState),
     });
 
     if (response.ok) {
       const data = await response.json();
       setMessage(data.message);
+      if(router) {
+        router.push({pathname:`/setGoals`, query:{}});
+      }
     } else {
       setMessage("Signup failed.");
     }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (router) {
-      router.push({pathname:`/setGoals`, query:{}});
-    }
-  };
   return (
     <div className={styles.signupContainer}>
       <h2>Signup to WorkoutWise</h2>
       <form onSubmit={handleSubmit}>
-        <label className={styles.signupLabel}>
-          Name:
-          <input
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-            className={styles.signupInput}
-          />
-        </label>
-
-        <label className={styles.signupLabel}>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            className={styles.signupInput}
-          />
-        </label>
-        <label className={styles.signupLabel}>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className={styles.signupInput}
-          />
-        </label>
-        <button
-          type="submit"
-          className={styles.signupButton}
-          onClick={handleSubmit}
-        >
+        <FormInput
+          label="Name"
+          type="text"
+          value={formState.username}
+          onChange={handleInputChange}
+          className={styles.signupInput}
+          name="username"
+        />
+        <FormInput
+          label="Email"
+          type="email"
+          value={formState.email}
+          onChange={handleInputChange}
+          className={styles.signupInput}
+          name="email"
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          value={formState.password}
+          onChange={handleInputChange}
+          className={styles.signupInput}
+          name="password"
+        />
+        <button type="submit" className={styles.signupButton}>
           Signup
         </button>
       </form>
