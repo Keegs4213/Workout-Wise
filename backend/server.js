ba54bec48b7bdf252a0cf136db04d06766bb94e7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-07-23 11:42:29 +1200

    profile upload fix

--- backend/server.js
+++ backend/server.js
@@ -1,75 +1,72 @@
 //server.js
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const userRoute = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute");
-const quoteRoute = require("./routes/quoteRoute")
-const multer = require("multer");
-require("dotenv").config();
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+const userRoute = require('./routes/userRoute')
+const authRoute = require('./routes/authRoute')
+const quoteRoute = require('./routes/quoteRoute')
+const multer = require('multer')
+require('dotenv').config()
 
-const app = express();
-const PORT = 3245;
+const app = express()
+const PORT = 3245
 
 // Connect to the database
-require("./database/mongodb");
+require('./database/mongodb')
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.use("/users", userRoute);
-app.use("/auth", authRoute);
-app.use("/quote", quoteRoute)
+app.use('/users', userRoute)
+app.use('/auth', authRoute)
+app.use('/quote', quoteRoute)
 
-
-const uploadPath = path.join(__dirname, '..', 'uploads')
-app.use(express.static(uploadPath));
-
+const uploadPath = path.join(__dirname, '..', 'front-end/public/upload')
+app.use(express.static(uploadPath))
+console.log('Upload path:', uploadPath)
 // multer
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     try {
-      cb(null, "uploads");
+      cb(null, 'front-end/public/upload')
     } catch (error) {
-      cb(error);
+      cb(error)
     }
   },
   filename(req, file, cb) {
     try {
-    cb(
+      cb(
         null,
-        `${file.fieldname}-${Date.now()}${path
-          .extname(file.originalname)}`
-      );
-     
+        `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+      )
     } catch (error) {
-      cb(error);
+      cb(error)
     }
   },
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
-app.post("/upload", upload.single("image"), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
   if (!req.file) {
-    return res.status(400).json({ message: "No photo uploaded" });
+    return res.status(400).json({ message: 'No photo uploaded' })
   }
   try {
     res.status(200).json({
-      message: "Photo successfully uploaded",
-      filePath: `/uploads/${req.file.filename}`
-    });
+      message: 'Photo successfully uploaded',
+      filePath: `/upload/${req.file.filename}`,
+    })
   } catch (error) {
     console.log(error)
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Server connect to PORT:${PORT}`);
-});
+  console.log(`Server connect to PORT:${PORT}`)
+})
 
-app.get("/", (req, res) => {
-  res.json({ message: "Connected to server" });
-});
+app.get('/', (req, res) => {
+  res.json({ message: 'Connected to server' })
+})
--- front-end/app/components/SignupForm.jsx
+++ front-end/app/components/SignupForm.jsx
@@ -1,108 +1,103 @@
 //components/SignupForm.jsx
-import { useState, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
-import { useRouter } from "next/router";
-import styles from "./../../app/globals.module.css";
-import "../globals.css"
+import { useState, useEffect } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useRouter } from 'next/router'
+import styles from './../../app/globals.module.css'
+import '../globals.css'
 
 function SignupForm() {
   const [formState, setFormState] = useState({
-    name: "",
-    password: "",
-    email: "",
-  });
-  const [message, setMessage] = useState("");
-  const [emailTaken, setEmailTaken] = useState(false);
-  const [isClient, setIsClient] = useState(false);
-  const router = isClient ? useRouter() : undefined;
+    name: '',
+    password: '',
+    email: '',
+  })
+  const [message, setMessage] = useState('')
+  const [emailTaken, setEmailTaken] = useState(false)
+  const [isClient, setIsClient] = useState(false)
+  const router = useRouter()
 
   useEffect(() => {
-    setIsClient(true);
-  }, []);
+    setIsClient(true)
+  }, [])
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormState((prevState) => ({ ...prevState, [name]: value }));
-  };
+    const { name, value } = event.target
+    setFormState((prevState) => ({ ...prevState, [name]: value }))
+  }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const response = await fetch("http://localhost:3245/auth/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('http://localhost:3245/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formState),
-    });
+    })
 
     if (response.ok) {
-      const data = await response.json();
-      setMessage(data.message);
+      const data = await response.json()
+      setMessage(data.message)
       // Store user's ID and name in local storage
-      localStorage.setItem('userId', data.response._id);
-      localStorage.setItem('userName', data.response.name);
+      localStorage.setItem('userId', data.response._id)
+      localStorage.setItem('userName', data.response.name)
       if (router) {
-        router.push({ pathname: `/setGoals`, query: {} });
+        router.push({ pathname: `/setGoals`, query: {} })
       }
     } else {
       if (response.status === 400) {
-        setEmailTaken(true);
-        const data = await response.json();
-        setMessage(data.message);
+        setEmailTaken(true)
+        const data = await response.json()
+        setMessage(data.message)
       } else {
-        setMessage("Signup failed.");
+        setMessage('Signup failed.')
       }
     }
-  };
+  }
 
   return (
-<<<<<<< HEAD
-    <div>
-      <h2>Signup</h2>
-=======
-    <div  className={styles.signupContainer}>
+    <div className={styles.signupContainer}>
       <h2 className={styles.loginLabel}>Signup</h2>
->>>>>>> development
       <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
+        <Form.Group className='mb-3'>
           <Form.Label>Name:</Form.Label>
           <Form.Control
-            type="text"
+            type='text'
             value={formState.name}
             onChange={handleInputChange}
-            name="name"
+            name='name'
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className='mb-3'>
           <Form.Label>Email:</Form.Label>
           <Form.Control
-            type="email"
+            type='email'
             value={formState.email}
             onChange={handleInputChange}
-            name="email"
+            name='email'
             isInvalid={emailTaken}
             required
           />
-          <Form.Control.Feedback type="invalid">
+          <Form.Control.Feedback type='invalid'>
             Sorry, that email address is taken. Try another?
           </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className='mb-3'>
           <Form.Label>Password:</Form.Label>
           <Form.Control
-            type="password"
+            type='password'
             value={formState.password}
             onChange={handleInputChange}
-            name="password"
+            name='password'
           />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant='primary' type='submit'>
           Signup
         </Button>
       </Form>
       <p>{message}</p>
     </div>
-  );
+  )
 }
 
-export default SignupForm;
+export default SignupForm
